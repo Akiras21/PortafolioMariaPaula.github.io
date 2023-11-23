062ed79c96856bcb6d2b2b1f698a84b297bcac10
Paula Granados <136767669+Akiras21@users.noreply.github.com>
2023-11-23 14:38:41 -0600

    Main Commit

---
+++ JS/Script.js
@@ -0,0 +1,28 @@
+document.getElementById ("btnCurrim").addEventListener('click', function (){
+    
+    window.open ("Curriculum Vitae María Paula Granados P..pdf", '_blank');
+});
+function bienv() {
+    var seccionDestino = document.getElementById('bienv');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
+function ttAcerca() {
+    var seccionDestino = document.getElementById('ttAcerca');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
+function secProjects() {
+    var seccionDestino = document.getElementById('secProjects');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
+function secSkills() {
+    var seccionDestino = document.getElementById('secSkills');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
+function formacionh3() {
+    var seccionDestino = document.getElementById('formacionh3');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
+function iconosContacto() {
+    var seccionDestino = document.getElementById('iconosContacto');
+    seccionDestino.scrollIntoView({ behavior: 'smooth' });
+}
